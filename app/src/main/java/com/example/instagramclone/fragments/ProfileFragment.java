9531a2c678a2cac0f50e0bf6bbeec826104deb31
Kaitlin <kaitlin.tranfl@gmail.com>
2021-03-01 23:12:39 -0500

    fixed issue with profile fragment

--- app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.java
@@ -15,24 +15,21 @@ public class ProfileFragment extends PostsFragment {
     protected void queryPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
-        query.setLimit(20);
         query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.setLimit(20);
         query.addDescendingOrder(Post.KEY_CREATED_AT);
         query.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
                 if (e != null) {
                     Log.e(TAG, "Issue with getting posts", e);
-                    e.printStackTrace();
                     return;
                 }
-
-                mPosts.addAll(posts);
-                adapter.notifyDataSetChanged();
-
                 for (Post post : posts) {
                     Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
                 }
+                mPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
             }
         });
     }
